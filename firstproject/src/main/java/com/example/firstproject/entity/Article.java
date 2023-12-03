7d85a8e70da26f36c7821eb9771bae097a951427
cheonyeonsu <cheonyeonsu@gmail.com>
2023-12-03 23:00:41 +0900

    ~4day

---
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,23 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue //자동 생성 기능 추가
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+
+
+}
