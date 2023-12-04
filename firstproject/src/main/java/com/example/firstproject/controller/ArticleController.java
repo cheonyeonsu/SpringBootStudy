556f3417b826cb6ea6930d83693ecd2975868ef9
cheonyeonsu <cheonyeonsu@gmail.com>
2023-12-04 15:22:38 +0900

    day5

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,13 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
+
 @Slf4j //로깅 기능 사용
 @Controller
 public class ArticleController {
@@ -33,5 +38,18 @@ public class ArticleController {
         log.info(saved.toString());
         //System.out.println(saved.toString());
         return "";
+    }
+    @GetMapping("/articles/{id}") //데이터 조회 요청 접수
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id); //id를 잘 받았는지 확인하는 로그 찍기.
+        //1. id를 조회해 데이터 가져오기
+       Article articleEntity = articleRepository.findById(id).orElse(null); //데이터 조회 후 값이 있으면 articleEntity변수에 값을 넣음.
+
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity); //article이라는 이름으로 articleEntity객체 추가.
+
+        //3. 뷰 페이지 반환하기
+        return "articles/show";
+
     }
 }
